768c244a7b857995b32e964685b705e7de1bb929
adsingh14 <deep.amansingh14@gmail.com>
2018-08-13 01:23:49 +0530

    Added style & effect to sticky button.

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Toucans-Team-20
+
 Voog Website Clone ([:link:](https://chingu-voyage5.github.io/Toucans-Team-20)) | Voyage-5 | chingu.io
 ---
 ### Team members
+
 - [Aman](https://github.com/adsingh14)
 
 ### Project Description:
@@ -32,12 +34,12 @@ Clone of a website landing page.(Official link: https://www.voog.com)
 
 ### Todo
 
-- [x] Feature 1 - Convert sidebar with offset sidebar.
+- [x] Feature 1 - Make offset sidebar.
 - [x] Feature 2 - Replace slide transition scroll effect with fade.
 - [x] Feature 3 - Add intro section along with video button.
-- [ ] Feature 4 - 'Get Started' Sticky Button with show/hide effect
+- [x] Feature 4 - 'Get Started' Sticky Button with show/hide effect
 - [x] Feature 5 - Parallax (Images & Buttons)
 - [ ] Feature 6 - Navbar affix (responsive mode)
 - [ ] Feature 7 - Changing navbar logo & text of color each section along with affix.
 
-_We would love to hear your valuable suggestions/feedback._
+_I would like to hear your valuable suggestions/feedback._
--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,95 +1,107 @@
 $(document).ready(function() {
-  // Navbar affix
-  var navbar = $('.navbar');
-  var navbar_button = $('.navbar-toggler');
 
-  var section_6 = $('#contact');
+    // Navbar affix
+    var navbar = $('.navbar');
+    var navbar_button = $('.navbar-toggler');
 
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > section_6.offset().top) {
-      navbar_button.addClass('black');
-    } else {
-      navbar_button.removeClass('black');
-    }
-  });
+    // Offset Navbar 
+    var toggler = $("#v-toggler");
+    var sidebar = $('.sidebar');
 
-  // Background changer
-  var section_4 = $('#about');
-  var section_5 = $('.second-half');
-
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > section_4.offset().top - 500 && $(this).scrollTop() < section_5.offset().top - 300) {
-      section_4.css('background', 'url(dist/images/img3.jpg) top/cover fixed no-repeat');
-    }
-    else if ($(this).scrollTop() > section_4.offset().top - 500 && $(this).scrollTop() < section_5.offset().top + 500)
-    {
-      section_4.css('background', 'url(dist/images/img4.jpg) top/cover fixed no-repeat');
-    }
-    else {
-      section_4.css('background',' #000');
-    }
-  });
-
-  // TODO: Sticky button
-  var sticky_btn = $('.sticky-btn');
-
-
-  // Scroll Ease
-  $('.sidebar a[href*="#"], footer ul a[href*="#"]')
-    .click(function (event) {
-      if (
-        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-        &&
-        location.hostname == this.hostname
-      ) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-          event.preventDefault();
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 500, function () {
-            var $target = $(target);
-            $target.focus();
-            if ($target.is(":focus")) {
-              return false;
-            } else {
-              $target.attr('tabindex', '-1');
-              $target.focus();
-            };
-          });
-        }
-      }
+    toggler.on('click', function() {
+        sidebar.toggleClass('open');
+        $('body').toggleClass('toggle_sidebar');
     });
 
-  //Side Navbar
-  var toggler = $("#v-toggler");
-  var sidebar = $('.sidebar');
+    
+    // Background changer
+    var section_4 = $('#about');
+    var section_5 = $('.second-half');
 
-  toggler.on('click', function () {
-    sidebar.toggleClass('open');
-    $('body').toggleClass('toggle_sidebar');
-  });
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > section_4.offset().top - 500 && $(this).scrollTop() < section_5.offset().top - 300) {
+            section_4.css('background', 'url(dist/images/img3.jpg) top/cover fixed no-repeat');
+        } else if ($(this).scrollTop() > section_4.offset().top - 500 && $(this).scrollTop() < section_5.offset().top + 500) {
+            section_4.css('background', 'url(dist/images/img4.jpg) top/cover fixed no-repeat');
+        } else {
+            section_4.css('background', '#333');
+        }
+    });
+
+    // Changing button color
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > section_5.offset().top) {
+            sticky_btn.children().removeClass('bg-dark text-white');
+            sticky_btn.children().addClass('bg-white text-dark');
+        } else {
+            sticky_btn.children().removeClass('bg-white text-dark');
+            sticky_btn.children().addClass('bg-dark text-white');
+        }
+    });
+
+    // Button state from fixed to sticky
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > section_7.offset().top) {
+            sticky_btn.addClass('final_state');
+
+        } else {
+            sticky_btn.removeClass('final_state');
+            sticky_btn.addClass('temp_state_1');
+        }
+    });
+
+
+    // revert button color 
+    var section_6 = $('#support');
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > section_6.offset().top) {
+            sticky_btn.children().addClass('bg-dark text-white');
+            sticky_btn.children().removeClass('bg-white text-dark');
+        }
+    });
+
+    // Changing button state to fixed
+    var sticky_btn = $('.sticky-btn');
+    var section_7 = $('#contact');
+    var footer = $('footer');
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > section_4.offset().top - 110) {
+            sticky_btn.addClass('temp_state_1');
+        } else {
+            sticky_btn.removeClass('temp_state_1');
+        }
+    });
+
+
+    // Scroll Ease
+    $('.sidebar a[href*="#"], footer ul a[href*="#"]')
+        .click(function(event) {
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 500, function() {
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) {
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1');
+                            $target.focus();
+                        };
+                    });
+                }
+            }
+        });
+
+
 
 });
-
-  /* var scroll_pos = 0;
-  $(document).scroll(function() {
-    scroll_pos = $(this).scrollTop();
-    if (scroll_pos > 2300) {
-      $("#combined").css("background", "url('images/img6.jpg')");
-    }
-  });
-
-  var scroll_pos = 0;
-  $(document).scroll(function() {
-    scroll_pos = $(this).scrollTop();
-    if (scroll_pos > 595 && scroll_pos < 1930) {
-      $(".toggler").css("color", "#000");
-      $(".login-btn").css("color", "#000");
-    } else {
-      $(".toggler").css("color", "#fff");
-      $(".login-btn").css("color", "#fff");
-    }
-  });
-*/
