e7354a056d797e808e146c4209713a5537733c12
adsingh14 <deep.amansingh14@gmail.com>
2018-08-15 00:39:20 +0530

    Toggle sticky button on breakpoint

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -13,68 +13,68 @@ $(document).ready(function() {
         $('body').toggleClass('toggle_sidebar');
     });
 
-    
-    // Background changer
-    var section_4 = $('#about');
-    var section_5 = $('.second-half');
+    if ($(window).width() > 768) {
+        // Background changer
+        var section_4 = $('#about');
+        var section_5 = $('.second-half');
 
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > section_4.offset().top - 500 && $(this).scrollTop() < section_5.offset().top - 300) {
-            section_4.css('background', 'url(dist/images/img3.jpg) top/cover fixed no-repeat');
-        } else if ($(this).scrollTop() > section_4.offset().top - 500 && $(this).scrollTop() < section_5.offset().top + 500) {
-            section_4.css('background', 'url(dist/images/img4.jpg) top/cover fixed no-repeat');
-        } else {
-            section_4.css('background', '#333');
-        }
-    });
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > section_4.offset().top - 500 && $(this).scrollTop() < section_5.offset().top - 300) {
+                section_4.css('background', 'url(dist/images/img3.jpg) top/cover fixed no-repeat');
+            } else if ($(this).scrollTop() > section_4.offset().top - 500 && $(this).scrollTop() < section_5.offset().top + 500) {
+                section_4.css('background', 'url(dist/images/img4.jpg) top/cover fixed no-repeat');
+            } else {
+                section_4.css('background', '#333');
+            }
+        });
 
-    // Changing button color
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > section_5.offset().top) {
-            sticky_btn.children().removeClass('bg-dark text-white');
-            sticky_btn.children().addClass('bg-white text-dark');
-        } else {
-            sticky_btn.children().removeClass('bg-white text-dark');
-            sticky_btn.children().addClass('bg-dark text-white');
-        }
-    });
+        // Changing button color
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > section_5.offset().top) {
+                sticky_btn.children().removeClass('bg-dark text-white');
+                sticky_btn.children().addClass('bg-white text-dark');
+            } else {
+                sticky_btn.children().removeClass('bg-white text-dark');
+                sticky_btn.children().addClass('bg-dark text-white');
+            }
+        });
 
-    // Button state from fixed to sticky
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > section_7.offset().top) {
-            sticky_btn.addClass('final_state');
+        // Button state from fixed to sticky
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > section_7.offset().top) {
+                sticky_btn.addClass('final_state');
 
-        } else {
-            sticky_btn.removeClass('final_state');
-            sticky_btn.addClass('temp_state_1');
-        }
-    });
-
-
-    // revert button color 
-    var section_6 = $('#support');
-
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > section_6.offset().top) {
-            sticky_btn.children().addClass('bg-dark text-white');
-            sticky_btn.children().removeClass('bg-white text-dark');
-        }
-    });
-
-    // Changing button state to fixed
-    var sticky_btn = $('.sticky-btn');
-    var section_7 = $('#contact');
-    var footer = $('footer');
-
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > section_4.offset().top - 110) {
-            sticky_btn.addClass('temp_state_1');
-        } else {
-            sticky_btn.removeClass('temp_state_1');
-        }
-    });
+            } else {
+                sticky_btn.removeClass('final_state');
+                sticky_btn.addClass('temp_state_1');
+            }
+        });
 
 
+        // revert button color 
+        var section_6 = $('#support');
+
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > section_6.offset().top) {
+                sticky_btn.children().addClass('bg-dark text-white');
+                sticky_btn.children().removeClass('bg-white text-dark');
+            }
+        });
+
+        // Changing button state to fixed
+        var sticky_btn = $('.sticky-btn');
+        var section_7 = $('#contact');
+        var footer = $('footer');
+
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > section_4.offset().top - 110) {
+                sticky_btn.addClass('temp_state_1');
+            } else {
+                sticky_btn.removeClass('temp_state_1');
+            }
+        });
+
+    }
     // Scroll Ease
     $('.sidebar a[href*="#"], footer ul a[href*="#"]')
         .click(function(event) {
@@ -101,7 +101,4 @@ $(document).ready(function() {
                 }
             }
         });
-
-
-
 });
